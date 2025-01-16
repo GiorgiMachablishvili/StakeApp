e2f5d9ed0e69adb66d56df6ab2d1ae0fe6d11add
GiorgiMachablishvili <g.machablishvili@yahoo.com>
2025-01-16 19:21:17 +0400

    Initial commit

---
+++ StakeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StakeApp
+//
+//  Created by Gio's Mac on 16.01.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+

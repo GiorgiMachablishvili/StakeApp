aefd78cc599ec7b17de7d3e6d94748894541c048
Tengiz Kiknadze <tengizkiknadze8@gmail.com>
2025-04-14 15:41:43 +0400

    asdksdfsdf

--- StakeApp/App/AppDelegate.swift
+++ StakeApp/App/AppDelegate.swift
@@ -1,29 +1,59 @@
 import UIKit
+import Firebase
+import FirebaseMessaging
+import UserNotifications
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate, MessagingDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-
-        UINavigationBar.appearance().isHidden = true
-
+        FirebaseApp.configure()
+        UNUserNotificationCenter.current().delegate = self
+        Messaging.messaging().delegate = self
+        requestPushPermissionsIfNeeded()
         return true
     }
 
-    // MARK: UISceneSession Lifecycle
+    func requestPushPermissionsIfNeeded() {
+        let alreadySent = UserDefaults.standard.bool(forKey: "pushTokenSent")
 
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+        guard !alreadySent else { return }
+
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { granted, _ in
+            DispatchQueue.main.async {
+                UIApplication.shared.registerForRemoteNotifications()
+            }
+        }
     }
 
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        Messaging.messaging().apnsToken = deviceToken
     }
 
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        guard let token = fcmToken, !token.isEmpty else { return }
+
+        let alreadySent = UserDefaults.standard.bool(forKey: "pushTokenSent")
+        guard !alreadySent else { return }
+
+        sendTokenToBackend(token) {
+            UserDefaults.standard.set(true, forKey: "pushTokenSent")
+        }
+    }
+
+    func sendTokenToBackend(_ token: String, completion: @escaping () -> Void) {
+        guard let url = URL(string: "https://bovagames.fun/fcm/register") else { return }
+
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+
+        let payload = ["token": token]
+        request.httpBody = try? JSONSerialization.data(withJSONObject: payload)
+
+        URLSession.shared.dataTask(with: request) { _, _, _ in
+            completion()
+        }.resume()
+    }
 
 }
-

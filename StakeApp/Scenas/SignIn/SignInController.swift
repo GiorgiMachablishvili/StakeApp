013273968a66e51ebca663ec2bbc568b8b05d2d9
user274877 <user274877@FF377.local>
2025-04-14 15:41:29 +0200

    from server

--- StakeApp/Scenas/SignIn/SignInController.swift
+++ StakeApp/Scenas/SignIn/SignInController.swift
@@ -12,7 +12,7 @@ class SignInController: UIViewController {
         view.contentMode = .scaleAspectFill
         return view
     }()
-
+    
     private lazy var signTitle: UILabel = {
         let view = UILabel(frame: .zero)
         view.text = "SIGN IN"
@@ -22,7 +22,7 @@ class SignInController: UIViewController {
         view.numberOfLines = 1
         return view
     }()
-
+    
     private lazy var signInInfoLabel: UILabel = {
         let view = UILabel(frame: .zero)
         view.text = "Sign in with your Apple account to transfer your account data and keep it safe."
@@ -32,29 +32,29 @@ class SignInController: UIViewController {
         view.numberOfLines = 0
         return view
     }()
-
+    
     private lazy var signInWithAppleButton: UIButton = {
         let view = UIButton(frame: .zero)
-            view.setTitle("Sign in with apple", for: .normal)
+        view.setTitle("Sign in with apple", for: .normal)
         view.setTitleColor(UIColor.titlesBlack, for: .normal)
         view.backgroundColor = UIColor.whiteColor
-            view.makeRoundCorners(16)
+        view.makeRoundCorners(16)
         view.titleLabel?.font = UIFont.montserratBlack(size: 12)
         view.layer.borderColor = UIColor.whiteColor.cgColor
-            view.layer.borderWidth = 1
-            let image = UIImage(named: "apple")?.withRenderingMode(.alwaysOriginal)
-            let resizedImage = UIGraphicsImageRenderer(size: CGSize(width: 24, height: 24)).image { _ in
-                image?.draw(in: CGRect(origin: .zero, size: CGSize(width: 24, height: 24)))
-            }
-            view.setImage(resizedImage, for: .normal)
-            view.imageView?.contentMode = .scaleAspectFit
-            view.imageEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 8)
-            view.titleEdgeInsets = UIEdgeInsets(top: 0, left: 8, bottom: 0, right: 0)
-            view.contentHorizontalAlignment = .center
-            view.addTarget(self, action: #selector(clickSignInWithAppleButton), for: .touchUpInside)
-            return view
+        view.layer.borderWidth = 1
+        let image = UIImage(named: "apple")?.withRenderingMode(.alwaysOriginal)
+        let resizedImage = UIGraphicsImageRenderer(size: CGSize(width: 24, height: 24)).image { _ in
+            image?.draw(in: CGRect(origin: .zero, size: CGSize(width: 24, height: 24)))
+        }
+        view.setImage(resizedImage, for: .normal)
+        view.imageView?.contentMode = .scaleAspectFit
+        view.imageEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 8)
+        view.titleEdgeInsets = UIEdgeInsets(top: 0, left: 8, bottom: 0, right: 0)
+        view.contentHorizontalAlignment = .center
+        view.addTarget(self, action: #selector(clickSignInWithAppleButton), for: .touchUpInside)
+        return view
     }()
-
+    
     private lazy var skipButton: UIButton = {
         let view = UIButton(frame: .zero)
         view.setTitle("Skip", for: .normal)
@@ -63,7 +63,7 @@ class SignInController: UIViewController {
         view.addTarget(self, action: #selector(clickSkipButton), for: .touchUpInside)
         return view
     }()
-
+    
     private lazy var termsButton: UIButton = {
         let view = UIButton(frame: .zero)
         let attributedTitle = NSAttributedString(
@@ -79,7 +79,7 @@ class SignInController: UIViewController {
         view.addTarget(self, action: #selector(clickTermsButton), for: .touchUpInside)
         return view
     }()
-
+    
     private lazy var privacyPolicyButton: UIButton = {
         let view = UIButton(frame: .zero)
         let attributedTitle = NSAttributedString(
@@ -95,17 +95,14 @@ class SignInController: UIViewController {
         view.addTarget(self, action: #selector(clickPrivacyPolicyButton), for: .touchUpInside)
         return view
     }()
-
-
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.mainViewsBackViewBlack
-
         setup()
         setupConstraints()
     }
-
+    
     private func setup() {
         view.addSubview(mainImageConsole)
         view.addSubview(signTitle)
@@ -115,54 +112,46 @@ class SignInController: UIViewController {
         view.addSubview(termsButton)
         view.addSubview(privacyPolicyButton)
     }
-
+    
     private func setupConstraints() {
         mainImageConsole.snp.remakeConstraints { make in
-//            make.top.leading.trailing.equalToSuperview()
             make.top.equalTo(view.snp.top).offset(120 * Constraint.yCoeff)
             make.leading.equalTo(view.snp.leading).offset(-20 * Constraint.xCoeff)
             make.trailing.equalTo(view.snp.trailing).offset(20 * Constraint.xCoeff)
             make.height.equalTo(380 * Constraint.yCoeff)
         }
-
         signTitle.snp.remakeConstraints { make in
             make.top.equalTo(view.snp.top).offset(368 * Constraint.yCoeff)
             make.leading.trailing.equalToSuperview().inset(32 * Constraint.xCoeff)
             make.height.equalTo(39 * Constraint.yCoeff)
         }
-
         signInInfoLabel.snp.remakeConstraints { make in
             make.top.equalTo(signTitle.snp.bottom).offset(8 * Constraint.yCoeff)
             make.leading.trailing.equalToSuperview().inset(32 * Constraint.xCoeff)
             make.height.equalTo(60 * Constraint.yCoeff)
         }
-
         signInWithAppleButton.snp.remakeConstraints { make in
             make.bottom.equalTo(view.snp.bottom).offset(-150 * Constraint.yCoeff)
             make.leading.trailing.equalToSuperview().inset(20 * Constraint.xCoeff)
             make.height.equalTo(52 * Constraint.yCoeff)
         }
-
         skipButton.snp.remakeConstraints { make in
             make.top.equalTo(signInWithAppleButton.snp.bottom).offset(8 * Constraint.yCoeff)
             make.leading.trailing.equalToSuperview().inset(20 * Constraint.xCoeff)
             make.height.equalTo(52 * Constraint.yCoeff)
         }
-
         termsButton.snp.remakeConstraints { make in
             make.bottom.equalTo(view.snp.bottom).offset(-32 * Constraint.yCoeff)
             make.leading.equalTo(view.snp.leading).offset(20 * Constraint.xCoeff)
             make.height.equalTo(15 * Constraint.yCoeff)
         }
-
         privacyPolicyButton.snp.remakeConstraints { make in
             make.centerY.equalTo(termsButton.snp.centerY)
             make.trailing.equalTo(view.snp.trailing).offset(-20 * Constraint.xCoeff)
             make.height.equalTo(15 * Constraint.yCoeff)
         }
-
     }
-
+    
     @objc private func clickTermsButton() {
         openHelper(url: "https://bovagames.fun/terms") { [weak self] button in
             switch button {
@@ -173,7 +162,7 @@ class SignInController: UIViewController {
             }
         }
     }
-
+    
     @objc private func clickPrivacyPolicyButton() {
         openHelper(url: "https://bovagames.fun/privacy") { [weak self] button in
             switch button {
@@ -184,60 +173,39 @@ class SignInController: UIViewController {
             }
         }
     }
-
+    
     @objc private func clickSkipButton() {
         UserDefaults.standard.setValue(true, forKey: "isGuestUser")
-
         UserDefaults.standard.removeObject(forKey: "userId")
         UserDefaults.standard.removeObject(forKey: "AccountCredential")
-
         let mainViewTabBarController = MainViewControllerTab()
         navigationController?.pushViewController(mainViewTabBarController, animated: true)
     }
-
+    
     @objc private func clickSignInWithAppleButton() {
-        // Simulating tokens for testing
-        let mockPushToken = "mockPushToken"
-        let mockAppleToken = "mockAppleToken"
-
-        // Store mock tokens in UserDefaults
-        UserDefaults.standard.setValue(mockPushToken, forKey: "PushToken")
-        UserDefaults.standard.setValue(mockAppleToken, forKey: "AccountCredential")
-
-        // Call createUser to simulate user creation
-        createUser()
-
-//        let authorizationProvider = ASAuthorizationAppleIDProvider()
-//        let request = authorizationProvider.createRequest()
-//        request.requestedScopes = [.email, .fullName]
-//
-//        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
-//        authorizationController.delegate = self
-//        authorizationController.performRequests()
-//        let mainView = MainDashboardScene()
-//        navigationController?.pushViewController(mainView, animated: true)
+        let authorizationProvider = ASAuthorizationAppleIDProvider()
+        let request = authorizationProvider.createRequest()
+        request.requestedScopes = [.email, .fullName]
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        authorizationController.delegate = self
+        authorizationController.presentationContextProvider = self
+        authorizationController.performRequests()
     }
-
+    
     private func createUser() {
         NetworkManager.shared.showProgressHud(true, animated: true)
-
         let pushToken = UserDefaults.standard.string(forKey: "PushToken") ?? ""
         let appleToken = UserDefaults.standard.string(forKey: "AccountCredential") ?? ""
-
-        // Prepare parameters
         let parameters: [String: Any] = [
             "push_token": pushToken,
             "apple_token": appleToken,
         ]
-
-        // Make the network request
         NetworkManager.shared.post(
             url: String.userCreate(),
             parameters: parameters,
             headers: nil
         ) { [weak self] (result: Result<UserCreate>) in
             guard let self = self else { return }
-
             DispatchQueue.main.async {
                 NetworkManager.shared.showProgressHud(false, animated: false)
                 UserDefaults.standard.setValue(false, forKey: "isGuestUser")
@@ -249,7 +217,6 @@ class SignInController: UIViewController {
                     print("User created: \(userInfo)")
                     UserDefaults.standard.setValue(userInfo.id, forKey: "userId")
                     print("Received User ID: \(userInfo.id)")
-
                     if let sceneDelegate = UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate {
                         let mainViewController = MainViewControllerTab()
                         let navigationController = UINavigationController(rootViewController: mainViewController)
@@ -264,7 +231,7 @@ class SignInController: UIViewController {
             }
         }
     }
-
+    
     private func showAlert(title: String, description: String) {
         let alert = UIAlertController(title: title, message: description, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "OK", style: .default))
@@ -272,49 +239,36 @@ class SignInController: UIViewController {
     }
 }
 
-extension SignInController: ASAuthorizationControllerDelegate /*ASAuthorizationControllerPresentationContextProviding*/ {
+extension SignInController: ASAuthorizationControllerDelegate, ASAuthorizationControllerPresentationContextProviding {
     func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
         guard let credential = authorization.credential as? ASAuthorizationAppleIDCredential else { return }
-
         UserDefaults.standard.setValue(credential.user, forKey: "AccountCredential")
-//        createUser()
+        createUser()
     }
-
-    //    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
-    //        print("Authorization failed: \(error.localizedDescription)")
-    //        showAlert(title: "Sign In Failed", description: error.localizedDescription)
-    //    }
+    
     func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
         let nsError = error as NSError
         if nsError.domain == ASAuthorizationError.errorDomain {
             switch nsError.code {
             case ASAuthorizationError.canceled.rawValue:
-                print("User canceled the Apple Sign-In process.")
-                // Optionally show a message or simply return
                 return
             case ASAuthorizationError.failed.rawValue:
-                print("Sign-In failed.")
                 showAlert(title: "Sign In Failed", description: "Something went wrong. Please try again.")
             case ASAuthorizationError.invalidResponse.rawValue:
-                print("Invalid response from Apple Sign-In.")
                 showAlert(title: "Invalid Response", description: "We couldn't authenticate you. Please try again.")
             case ASAuthorizationError.notHandled.rawValue:
-                print("Apple Sign-In not handled.")
                 showAlert(title: "Not Handled", description: "The request wasn't handled. Please try again.")
             case ASAuthorizationError.unknown.rawValue:
-                print("An unknown error occurred.")
                 showAlert(title: "Unknown Error", description: "An unknown error occurred. Please try again.")
             default:
                 break
             }
         } else {
-            print("Authorization failed with error: \(error.localizedDescription)")
             showAlert(title: "Sign In Failed", description: error.localizedDescription)
         }
     }
-
+    
     func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
         return self.view.window!
     }
 }
-
